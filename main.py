b95d24731559a3bbc4d033a6b158592311ddc63c
mjy8086 <mjy8086@korea.ac.kr>
2021-11-12 14:52:39 +0900

    update

--- main.py
+++ main.py
@@ -15,22 +15,34 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # making model
 class MultivariateLinearRegressionModel(nn.Module):
-    def __init__(self, in_features, hidden_features, out_features):
+    def __init__(self, in_features, out_features):
         super().__init__()
-        self.fc1 = nn.Linear(in_features, hidden_features)
-        self.fc2 = nn.Linear(hidden_features, hidden_features)
-        self.fc3 = nn.Linear(hidden_features, hidden_features)
-        self.fc4 = nn.Linear(hidden_features, hidden_features)
-        self.fc5 = nn.Linear(hidden_features, hidden_features)
-        self.fc6 = nn.Linear(hidden_features, out_features)
+        self.fc1 = nn.Linear(in_features, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 128)
+        self.fc4 = nn.Linear(128, 128)
+        # self.fc5 = nn.Linear(128, 128)
+        self.fc6 = nn.Linear(128, out_features)
+        self.batchnorm = nn.BatchNorm1d(128)
 
     def forward(self, x):
         h1 = F.leaky_relu(self.fc1(x))
+        h1 = self.batchnorm(h1)
+        # h1 = nn.Dropout(0.1)(h1)
         h2 = F.leaky_relu(self.fc2(h1))
+        h2 = self.batchnorm(h2)
+        # h2 = nn.Dropout(0.1)(h2)
         h3 = F.leaky_relu(self.fc3(h2))
+        h3 = self.batchnorm(h3)
+        h3 = nn.Dropout(0.1)(h3)
         h4 = F.leaky_relu(self.fc4(h3))
-        h5 = F.leaky_relu(self.fc5(h4))
-        f1 = self.fc6(h5)
+        h4 = self.batchnorm(h4)
+        h4 = nn.Dropout(0.3)(h4)
+        # h5 = F.leaky_relu(self.fc5(h4))
+        # h5 = self.batchnorm(h5)
+        # h5 = nn.Dropout(0.1)(h5)
+        f1 = self.fc6(h4)
+        # f1 = nn.Dropout(0.1)(f1)
         out = torch.squeeze(f1)
 
         return out
@@ -40,6 +52,7 @@ class Dataset(Dataset):
     def __init__(self, dataset):
         self.data = pd.read_csv('data/' + dataset + '.csv')
 
+# 이건 이산형을 정수화하고 min/max로 정규화 한 버전
         encoder = LabelEncoder()
         scaler = MinMaxScaler(copy=True, feature_range=(0, 1))
 
@@ -47,17 +60,14 @@ class Dataset(Dataset):
         self.data['transmission'] = encoder.fit_transform(self.data['transmission'])
         self.data['fuelType'] = encoder.fit_transform(self.data['fuelType'])
 
-        self.data['year'] = scaler.fit_transform(self.data[['year']])
-        self.data['mileage'] = scaler.fit_transform(self.data[['mileage']])
-        self.data['tax'] = scaler.fit_transform(self.data[['tax']])
-        self.data['mpg'] = scaler.fit_transform(self.data[['mpg']])
-        self.data['engineSize'] = scaler.fit_transform(self.data[['engineSize']])
 
         self.x_train = self.data.drop('price', axis=1)
         self.y_train = self.data['price']
 
-        self.x_train = self.x_train.values
+        self.x_train = scaler.fit_transform(self.x_train.values)
         self.y_train = self.y_train.values
+# Train_MAE = 1576
+# AdamW = 1591
 
 
 # 이건 이산형을 정수화한 버전
@@ -79,7 +89,7 @@ class Dataset(Dataset):
 #
 #         self.x_train = self.x_train.values
 #         self.y_train = self.y_train.values
-
+# Train_MAE = 1730
 
     def __len__(self):
         return len(self.x_train)
@@ -93,11 +103,11 @@ class Dataset(Dataset):
 
 batch_size = 256
 in_features = 8
-hidden_features = 128
+# hidden_features = 128
 out_features = 1
 lr = 0.03
 weight_decay = 1e-7
-n_epochs = 500
+n_epochs = 150
 
 if __name__ == '__main__':
     train_data = Dataset('train')
@@ -105,7 +115,7 @@ if __name__ == '__main__':
     trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     validloader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
 
-    model = MultivariateLinearRegressionModel(in_features, hidden_features, out_features)
+    model = MultivariateLinearRegressionModel(in_features, out_features)
     model = model.cuda()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
