06722533765fd03ac4b7dfae3ede158b9010c3c0
mjy8086 <mjy8086@korea.ac.kr>
2021-11-12 16:07:05 +0900

    update

--- baseline.py
+++ baseline.py
@@ -8,6 +8,22 @@ import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+"""
+'Applications and Practice in Neural Networks'
+
+Project_No.9: Car Price Regression Model
+
+Baseline model
+RMSE loss for the test dataset: 
+"""
+
+
+
 
 
 class MultivariateLinearRegressionModel(nn.Module):
@@ -65,32 +81,62 @@ n_epochs = 200
 if __name__ == '__main__':
     train_data = Dataset('train')
     val_data = Dataset('val')
+    test_data = Dataset('test')
     trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     validloader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
+    testloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-    model = MultivariateLinearRegressionModel(in_features, hidden_features, out_features)
+    model = MultivariateLinearRegressionModel(in_features, hidden_features,out_features)
+    model = model.cuda()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
     for epoch in range(n_epochs + 1):
         train_loss = 0.0
+        train_mae = 0
         for batch_idx, (x_train, y_train) in enumerate(trainloader):
-            pred = model(x_train)
-            loss = F.mse_loss(pred, y_train)
+        # for batch_idx, (x_train, y_train) in tqdm(enumerate(trainloader)):
+
+            pred_train = model(x_train.cuda())
+            loss = F.mse_loss(pred_train, y_train.cuda()).cuda()
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             train_loss += np.sqrt(loss.item())
+            train_mae1 = torch.abs(pred_train - y_train.cuda()).sum() / batch_size
+            train_mae += train_mae1
+            # train_loss += loss.item()
 
         model.eval()
         with torch.no_grad():
             valid_loss = 0.0
+            valid_mae = 0
             for x_val, y_val in validloader:
-                pred = model(x_val)
-                loss = F.mse_loss(pred, y_val)
+                pred_val = model(x_val.cuda())
+                loss = F.mse_loss(pred_val, y_val.cuda())
                 valid_loss += np.sqrt(loss.item())
+                valid_mae1 = torch.abs(pred_val - y_val.cuda()).sum() / batch_size
+                valid_mae += valid_mae1
 
         if epoch % 10 == 0:
-            print('Epoch {:4d}/{} Train_Loss: {:.6f} Val_Loss: {:.6f}'.format(
+            print('Epoch {:4d}/{} Train_Loss: {:.3f} Val_Loss: {:.3f}'.format(
                 epoch, n_epochs, train_loss / len(trainloader), valid_loss / len(validloader)
             ))
+            print('Train_MAE: {:.3f} Val_MAE: {:.3f}'.format(
+                train_mae / len(trainloader), valid_mae / len(validloader)
+            ))
+
+    model.eval()
+    with torch.no_grad():
+        test_loss = 0.0
+        test_mae = 0
+        for x_test, y_test in testloader:
+            pred_test = model(x_test.cuda())
+            loss = F.mse_loss(pred_test, y_test.cuda())
+            test_loss += np.sqrt(loss.item())
+            test_mae1 = torch.abs(pred_test - y_test.cuda()).sum() / batch_size
+            test_mae += test_mae1
+
+    print('Test_Loss: {:.3f} Test_MAE: {:.3f}'.format(
+        test_loss / len(testloader), test_mae / len(testloader)
+    ))
